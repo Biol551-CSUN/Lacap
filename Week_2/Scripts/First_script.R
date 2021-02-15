8e93b6d25bc5647a248d17a400df69efb2db3df6
Roland <roland.lacap.270@my.csun.edu>
2021-02-14 23:36:43 -0800

    Updated comments from Jamie.

--- Week_2/Scripts/First_script.R
+++ Week_2/Scripts/First_script.R
@@ -7,9 +7,14 @@
 ### Load libraries ##############################
 library(tidyverse)
 library(here)
+
+
 ### Read in data ################################
 WeightData<-read_csv(here("Week_2","data","weightdata.csv"))
+
+
 ### Data Analysis ###############################
 head(WeightData) # Looks at the top 6 lines of the dataframe
 tail(WeightData) # Looks at the bottom 6 lines of the dataframe
 View(WeightData) # opens a new window to look at the entire dataframe
+
